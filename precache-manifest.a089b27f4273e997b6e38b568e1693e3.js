3384ace2975e4718b5532b4d4f366d8681d5baf4
DevAshleyD <webdevmvp@gmail.com>
2019-11-28 05:33:14 +0000

    Updates

---
+++ precache-manifest.a089b27f4273e997b6e38b568e1693e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc13ea5680410b7e467042c9870fc83",
+    "url": "/react-simon-says/index.html"
+  },
+  {
+    "revision": "9fa351fcfa75cab0f3b8",
+    "url": "/react-simon-says/static/css/main.d93ef70b.chunk.css"
+  },
+  {
+    "revision": "e93edbef83a8c781faaf",
+    "url": "/react-simon-says/static/js/2.88631e05.chunk.js"
+  },
+  {
+    "revision": "9fa351fcfa75cab0f3b8",
+    "url": "/react-simon-says/static/js/main.c6de1a6b.chunk.js"
+  },
+  {
+    "revision": "76f904416910f3b68907",
+    "url": "/react-simon-says/static/js/runtime-main.2d5f8da1.js"
+  },
+  {
+    "revision": "d6f8a4c1c4fce8d9ad93c0bd33b478b7",
+    "url": "/react-simon-says/static/media/blue.d6f8a4c1.mp3"
+  },
+  {
+    "revision": "3c8ca9470540bbbea8eaa187bfe82a39",
+    "url": "/react-simon-says/static/media/green.3c8ca947.mp3"
+  },
+  {
+    "revision": "8d481077276f07445e615252af9a8be4",
+    "url": "/react-simon-says/static/media/red.8d481077.mp3"
+  },
+  {
+    "revision": "3ee582459fb09df5b041ef1b9593d7b5",
+    "url": "/react-simon-says/static/media/yellow.3ee58245.mp3"
+  }
+]);
